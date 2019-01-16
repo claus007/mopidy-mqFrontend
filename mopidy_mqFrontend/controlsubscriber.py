ed2b669b4b437c841b1111611a53814069bd0285
Claus Ilginnis <claus@ilginnis.de>
2019-01-16 21:19:00 +0100

    implement on control some cody style correction

--- mopidy_mqFrontend/controlsubscriber.py
+++ mopidy_mqFrontend/controlsubscriber.py
@@ -15,51 +15,65 @@
 from __future__ import absolute_import, unicode_literals
 
 import pykka
-import mopidy
-import paho.mqtt.client as mqtt
+import paho.mqtt.client
 
 import logging
 
 
 class ControlSubscriber(pykka.ThreadingActor):
 
-    def __init__(self, config, core, logger):
+    config = None  # type: dict
+    core = None  # type: mopidy.core.Core
+    mqtt_client = None  # type: paho.mqtt.client.Client
+
+    def __init__(self, config, core, logger, *args, **kwargs):
+        super(ControlSubscriber, self).__init__(*args, **kwargs)
+        self.in_future = self.actor_ref.proxy()
         self.config = config
         self.core = core
         self.logger = logger
-        self.mqtt_client = mqtt()
+        self.mqtt_client = paho.mqtt.client.Client()
 
     def on_start(self):
-        host = self.config["host"]
-        port = self.config["port"]
+        host = self.config['host']
+        port = self.config['port']
         self.mqtt_client.on_connect = self.on_connect
         self.mqtt_client.on_disconnect = self.on_disconnect
-        self.mqtt_client.on_message = self.on_mqMseaage
+        self.mqtt_client.on_message = self.on_mq_message
 
-        self.logger.info("Starting Control Client / Connecting on %s:%d" % (host, port))
+        self.logger.info('Starting Control Client / Connecting on %s:%d' % (host, port))
         self.mqtt_client.connect(host, port)
 
+        self.in_future.do_work()
+
     def on_stop(self):
-        self.logger.info("Stopping Client - disabling reconnection")
+        self.logger.info('Stopping Client - disabling reconnection')
         self.mqtt_client.on_disconnect = None
         self.mqtt_client.disconnect()
-        self.logger.info("Client stopped")
-
+        self.logger.info('Client stopped')
 
     def on_connect(self):
-        self.logger.debug("Connected")
-        topic="{1}/{2}".format(self.config["topic"],"control")
-        self.logger.debug("Subscribing to {}".format(topic))
-        self.mqtt_client.subsribe(topic)
+        self.logger.debug('Connected')
+        topic = "{0}/{1}".format(self.config['topic'], 'control')
+        self.logger.debug('Subscribing to {}'.format(topic))
+        self.mqtt_client.subscribe(topic)
 
     def on_disconnect(self):
-        self.logger.debug("DISConnected - Reconnecting...")
+        self.logger.info('DISConnected - Reconnecting...')
         self.mqtt_client.reconnect()
 
-    def on_mqMseaage(self,mqttc, obj, msg):
-        if msg.payload="stop":
-            mopidy.core.
+    def on_mq_message(self, mqttc, obj, msg):
+        self.logger.info('Received msg: {}' % msg.payload)
+        if msg.payload == 'stop':
+            self.core.playlist.stop()
+            return
+        if msg.payload == 'start':
+            self.core.playlist.start()
+            return
+        if msg.payload == 'stop':
+            self.core.playlist.stop()
+            return
 
-
-
-
+    def do_work(self):
+        self.mqtt_client.loop()
+        self.in_future.do_work()
